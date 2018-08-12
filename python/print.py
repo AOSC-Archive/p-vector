7e4eae693e6cf412dadcfc13eda5fd43ebe3adc6
Daming Yang <lion@aosc.io>
2018-08-12 18:52:59 +0800

    python/print: add **kargs

--- python/print.py
+++ python/print.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+PROGRESS_BAR_LENGTH = 20
 
 def _block_display(v: float, length: int):
     table = ['\u2003', '\u258f', '\u258e', '\u258d', '\u258c', '\u258b', '\u258a', '\u2589', '\u2588']
@@ -14,26 +15,24 @@ def _block_display(v: float, length: int):
 
 
 def progress_bar(title: str, val: float):
-    print(title.ljust(20), _block_display(val, 30), end='\n\033[A')
+    print(title.ljust(20), _block_display(val, PROGRESS_BAR_LENGTH), end='\n\033[A')
 
 
 def progress_bar_end(title: str):
-    print(title.ljust(20), _block_display(1, 30), end='\n\033[A')
-    print()
+    print(title.ljust(20), _block_display(1, PROGRESS_BAR_LENGTH), end='\n\033[A\n')
 
 
-def I(comp: str, *args):
-    _print('I', comp, *args)
+def I(comp: str, *args, **kargs):
+    _print('I', comp, *args, **kargs)
 
 
-def E(comp: str, *args):
-    _print('E', comp, *args)
+def E(comp: str, *args, **kargs):
+    _print('E', comp, *args, **kargs)
 
 
-def W(comp: str, *args):
-    _print('W', comp, *args)
+def W(comp: str, *args, **kargs):
+    _print('W', comp, *args, **kargs)
 
 
-def _print(t: str, comp: str, *args):
-    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-          t, '[' + comp.ljust(5) + ']', *args)
+def _print(t: str, comp: str, *args, **kargs):
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), t, '[' + comp.ljust(5) + ']', *args, **kargs)
