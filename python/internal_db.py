d35654c2cb97715ab68a770bcf4c0f86c7764777
Tianhao Chai <cth451@gmail.com>
2019-08-15 04:29:45 -0500

    init_db: create pv_packages before using, fix #3

--- python/internal_db.py
+++ python/internal_db.py
@@ -109,8 +109,6 @@ def init_db(db):
                 ')')
     cur.execute('ALTER TABLE pv_repos ADD COLUMN IF NOT EXISTS '
                 'mtime TIMESTAMP WITH TIME ZONE')
-    cur.execute('UPDATE pv_repos r SET mtime=(SELECT to_timestamp(max(mtime)) '
-                'FROM pv_packages p WHERE p.repo=r.name) WHERE mtime IS NULL')
     cur.execute('CREATE TABLE IF NOT EXISTS pv_packages ('
                 'package TEXT,'
                 'version TEXT,'
@@ -128,6 +126,8 @@ def init_db(db):
                 '_vercomp TEXT,'
                 'PRIMARY KEY (package, version, repo)'
                 ')')
+    cur.execute('UPDATE pv_repos r SET mtime=(SELECT to_timestamp(max(mtime)) '
+                'FROM pv_packages p WHERE p.repo=r.name) WHERE mtime IS NULL')
     cur.execute('CREATE TABLE IF NOT EXISTS pv_package_duplicate ('
                 'package TEXT,'
                 'version TEXT,'
