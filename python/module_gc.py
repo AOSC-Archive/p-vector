90a54eb34dc6c607a5fc5499b337a2b137df43a0
Tianhao Chai <cth451@gmail.com>
2020-10-30 14:22:53 -0500

    gc: reindex after gc

--- python/module_gc.py
+++ python/module_gc.py
@@ -1,16 +1,10 @@
-import os
 from pathlib import PosixPath
 
 import logging
-import binascii
-import functools
-import urllib.parse
-import multiprocessing.dummy
-from subprocess import CalledProcessError
-
 from typing import List
 
 import module_config
+import internal_db
 
 logger_gc = logging.getLogger('GC')
 
@@ -18,9 +12,12 @@ logger_gc = logging.getLogger('GC')
 def purge_from_db(db, branch_component: List[str]):
     cur = db.cursor()
     for i in branch_component:
-        logger_gc.info("%s removed from database", i)
+        logger_gc.info("REMOVING branch %s from database", i)
         cur.execute("DELETE FROM pv_repos WHERE path = %s", (i, ))
     db.commit()
+    if branch_component:
+        logger_gc.info("Refreshing indices and materialized views")
+        internal_db.init_index(db, True)
     cur.close()
 
 
@@ -33,7 +30,7 @@ def run_gc(db, base_dir: str, dry_run: bool):
         path = PosixPath(pool_dir).joinpath(i)
         if not path.is_dir():
             to_delete.append(i)
-            logger_gc.info("%s will be removed from the database", i)
+            logger_gc.info("Branch %s to be removed from the database", i)
 
     if not dry_run:
         purge_from_db(db, to_delete)
