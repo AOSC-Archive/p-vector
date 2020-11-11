e8c0214d063b2a3cafdc83011f97c429b483f2f6
liushuyu <liushuyu011@gmail.com>
2020-11-11 03:39:56 -0700

    ci: set package version

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -ex
 
+# set version
+if [ -z "$GIT_TAG_NAME" ]; then
+  export PV_VERSION="0+git$(date +'%Y%m%d')"
+else
+  export PV_VERSION="$GIT_TAG_NAME"
+fi
+
 mkdir -p build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -GNinja
 ninja package
